792109d5f88b3b9dbd1d854c41837ab37da37648
Wasswa Jim Daniels <wasswa.jim@warchild.nl>
2021-09-30 11:56:05 +0300

    Added Static Files Config

--- urbanshop/urbanshop/urls.py
+++ urbanshop/urbanshop/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.conf.urls.static import static
+from django.conf import settings
 from shop.views import index
 
 urlpatterns = [
     path('', view = index, name = 'index'),
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
